79b2156c83524c69d91b311ca62b0bb1fe515141
Chris <christian2.harrhy@live.uwe.ac.uk>
2023-04-30 15:13:53 +0100

    Platforms working but slight issue with player on them
    Working in my solo project so will just need a slight adjustment to work in this I think

--- Assets/Scripts/Platforms/PlatformController.cs
+++ Assets/Scripts/Platforms/PlatformController.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class PlatformController : MonoBehaviour
 {
     public float speed = 2.0f;
-    public float distance = 2.0f; // distance platform moves up and down
-    public MovementType movementType; // choose the movement to use in the inspector
-
+    public float distance = 2.0f; // distance platforms move
+    public MovementType movementType;
     private Vector3 initialPosition;
-    private float direction = 1.0f; // direction of platform movement (up or down)
+    private float direction = 1.0f;
     private float angle = 0.0f; // current angle of platform in circle movement
-    private const float CIRCLE_RADIUS = 2.0f; // radius of circle for circle movement
+    private float cirleRadius = 2.0f;
+    public GameObject player;
+    public bool isOnPlatform = false;
+    public bool wasOnPlatform = false;
 
     public enum MovementType
     {
+        // Choose in inspector
         UpDown,
-        Circle
+        Circle,
+        LeftRight,
+        ForwardBackward
     }
 
     void Start()
     {
-        initialPosition = transform.position; // store initial position of platform
+        initialPosition = transform.position;
     }
 
     void Update()
@@ -34,15 +39,32 @@ public class PlatformController : MonoBehaviour
             case MovementType.Circle:
                 MoveInCircle();
                 break;
+            case MovementType.ForwardBackward:
+                MoveForwardBackward();
+                break;
+            case MovementType.LeftRight:
+                MoveLeftRight();
+                break;
         }
+
+        if (isOnPlatform && !wasOnPlatform)
+        {
+            player.transform.SetParent(this.transform);
+            Debug.Log("Player is child");
+        }
+
+        if (!isOnPlatform && wasOnPlatform)
+        {
+            player.transform.SetParent(null);
+        }
+
+        wasOnPlatform = isOnPlatform;
     }
 
     private void MoveUpDown()
     {
-        // move platform up or down based on current direction
         transform.position += direction * Vector3.up * speed * Time.deltaTime;
 
-        // if platform has moved its maximum distance, switch direction
         if (transform.position.y > initialPosition.y + distance)
         {
             direction = -1.0f;
@@ -58,11 +80,54 @@ public class PlatformController : MonoBehaviour
         angle += speed * Time.deltaTime; // increase angle based on speed
 
         // calculate new position based on angle and radius of circle
-        float x = initialPosition.x + CIRCLE_RADIUS * Mathf.Cos(angle);
-        float z = initialPosition.z + CIRCLE_RADIUS * Mathf.Sin(angle);
+        float x = initialPosition.x + cirleRadius * Mathf.Cos(angle);
+        float z = initialPosition.z + cirleRadius * Mathf.Sin(angle);
         float y = initialPosition.y;
 
         transform.position = new Vector3(x, y, z);
     }
-}
 
+    private void MoveLeftRight()
+    {
+        transform.position += direction * Vector3.right * speed * Time.deltaTime;
+
+        if (transform.position.x > initialPosition.x + distance)
+        {
+            direction = -1.0f;
+        }
+        else if (transform.position.x < initialPosition.x)
+        {
+            direction = 1.0f;
+        }
+    }
+
+    private void MoveForwardBackward()
+    {
+        transform.position += direction * Vector3.forward * speed * Time.deltaTime;
+
+        if (transform.position.z > initialPosition.z + distance)
+        {
+            direction = -1.0f;
+        }
+        else if (transform.position.z < initialPosition.z)
+        {
+            direction = 1.0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isOnPlatform = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isOnPlatform = false;
+        }
+    }
+}
