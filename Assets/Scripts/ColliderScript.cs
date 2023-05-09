5e7ef3b85e7f1f7f3f2e1e6bbbea10605ae614dd
Calvin Oktavialdy Setia <calvin2.setia@live.uwe.ac.uk>
2023-05-09 12:19:28 +0100

    spline set up. needs implementation on the player controls

---
+++ Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    // public
+    public bool collided;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collided = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collided = false;
+        }
+    }
+}
